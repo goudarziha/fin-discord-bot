1dc01faae07b026f3c03f889859dede8e907daa7
Amir <goudarziha@gmail.com>
2023-01-15 12:57:39 -0500

    fix docker-compose/docker, init celery

--- app/main.py
+++ app/main.py
@@ -1,9 +1,31 @@
 import asyncio
 import discord
-from fastapi import FastAPI
+
+from celery.result import AsyncResult
+from fastapi import FastAPI, Body, Request
+from fastapi.responses import JSONResponse
+
+from worker import create_task
 
 app = FastAPI()
 
 @app.get("/")
 async def read_root():
     return {"hello": "world"}
+
+@app.post("/tasks", status_code=201)
+def run_task(payload = Body(...)):
+    task_type = payload['type']
+    task = create_task.delay(int(task_type))
+    return JSONResponse({"task_id": task.id})
+
+@app.get("/tasks/{task_id}")
+def get_status(task_id):
+    task_result = AsyncResult(task_id)
+    result = {
+        "task_id": task_id,
+        "task_status": task_result.status,
+        "task_result": task_result.result
+    }
+    return JSONResponse(result)
+
--- app/worker.py
+++ app/worker.py
@@ -6,7 +6,7 @@ from celery import Celery
 
 celery = Celery(__name__)
 celery.conf.broker_url = os.environ.get("CELERY_BROKER_URL", "redis://localhost:6379")
-celery.conf.result_backend = os.enviorn.get("CELERY_RESULT_BACKEND", "redis://localhost:6379")
+celery.conf.result_backend = os.environ.get("CELERY_RESULT_BACKEND", "redis://localhost:6379")
 
 @celery.task(name="create_task")
 def create_task(task_type):
